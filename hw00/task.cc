e4234d077ae52c6ba16e7dad2d81cf6985c16947
AlexAI <zavrazin2006@mail.ru>
2025-09-26 19:41:50 +0700

    правки

--- hw00/task.cc
+++ hw00/task.cc
@@ -2,19 +2,23 @@
 #include <string>
 #include <vector>
 
-int main() {
-    std::string str = "abcdef";
-    std::vector<int> idx = {2, 0, 1, 5, 4, 3};
-
+std:: string f(std::string str, std:: vector<int> idx){
     int N = str.size();
     std::string result(N, ' ');
 
     for (int i = 0; i < N; i++) {
         result[idx[i]] = str[i];
     }
+    return result;
+}
+int main() {
+    std::string str = "abcdef";
+    std::vector<int> idx = {2, 0, 1, 5, 4, 3};
 
-    std::cout << "Исходная строка: " << str << std::endl;
-    std::cout << "Зашифрованная строка: " << result << std::endl;
+    
+
+    std::cout << str << std::endl;
+    std::cout << result << std::endl;
 
     return 0;
 }
